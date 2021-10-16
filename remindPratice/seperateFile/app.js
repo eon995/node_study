afda660d03bf388519bbd0f3611d244d710431cf
eon995 <84843589+eon995@users.noreply.github.com>
2021-10-16 11:27:11 +0900

    serperateFile pratice

--- remindPratice/seperateFile/app.js
+++ remindPratice/seperateFile/app.js
@@ -1,7 +1,21 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
+const baseDir = '/Users/fjr02/Desktop/playground';
+
+fs.readdir('/Users/fjr02/Desktop/playground', function (err, fileList) {
+
+    for (var i = 0; i < fileList.length; i++) {
+        let extname = path.extname(fileList[i]);
+        let name = fileList[i];
+        if (extname == '.url') {
+            fs.rename(baseDir + path.sep + name, baseDir + path.sep + '/game/' + name, (err) => {
+                console.log(err);
+            })
+        } else {
+
+        }
 
 
-fs.readdir('/Users/fjr02/Desktop/playground')
-    .then(console.log)
-    .catch(console.error);
+    }
+
+});
