d8e46c7d9ca38f4fa231393fd6d506a79f6ac32e
eon995 <84843589+eon995@users.noreply.github.com>
2021-10-28 22:43:18 +0900

    jwt 복습

--- remindPratice/r-auth/2-jwt.js
+++ remindPratice/r-auth/2-jwt.js
@@ -3,6 +3,10 @@ const jwt = require('jsonwebtoken');
 const token = jwt.sign({
     id: 'userId',
     isAdmin: 'true'
-}, 'abc123');
+}, 'abc123', { expiresIn: 10 });
 
 console.log(token);
+
+jwt.verify(token, 'abc123', (error, decoded) => {
+    console.log(error, decoded);
+});
