56c60394d15c8cd238a9920f3d991dbbbb71f57c
eon995 <84843589+eon995@users.noreply.github.com>
2021-11-01 23:17:27 +0900

    create account 영문숫자 제한하기

--- rDwit/Router/auth.js
+++ rDwit/Router/auth.js
@@ -8,13 +8,38 @@ import { validate } from '../middleware/validate.js';
 
 const router = express.Router();
 
+const signupValidate =
+    [
+        body('id').notEmpty().trim().isLength({ min: 2, max: 10 }).matches(/[a-z0-9]/),
+        validate
+    ];
+
+
+const idRange = /[^a-z0-9]/g;
+
 router.post('/test', authController.test);
 
 // router.get('/me', authController.me);
 
-router.post('/login', authController.createAccount);
+// router.post('/login', authController.createAccount);
 
-// router.post('/signup', authController.signup);
+router.post('/signup', authController.createAccount);
+
+
+
+
+export function checkId(req, res, next) {
+    const id = /[^a-z0-9]/g;
+    const check = id.exec(req);
+
+    if (check) {
+        return res.status(400).withmessage("영문과 숫자만 사용가능합니다.");
+
+    }
+    return next();
+
+}
+
 
 export default router;
 
