3c713a01b53266bd3e89000e4aefaa85cc07a7b1
eon995 <84843589+eon995@users.noreply.github.com>
2021-11-02 23:41:59 +0900

    회원가입 유효성검사

--- rDwit/Router/auth.js
+++ rDwit/Router/auth.js
@@ -11,6 +11,11 @@ const router = express.Router();
 const signupValidate =
     [
         body('id').notEmpty().trim().isLength({ min: 2, max: 10 }).withMessage("2글자이상 10글자이하로 입력해주세요").matches(/^[a-z0-9_-]/).withMessage("소문자 영문 및 숫자만 사용해주세요."),
+        body('password').notEmpty().trim().isLength({ min: 2, max: 10 }).withMessage("2글자이상 10글자이하로 입력해주세요"),
+        body('name').notEmpty().trim().isLength({ min: 2, max: 20 }).withMessage("2글자이상 20글자이하로 입력해주세요"),
+        body('username').notEmpty().trim().isLength({ min: 2, max: 20 }).withMessage("2글자이상 20글자이하로 입력해주세요"),
+        body('email').isEmail().withMessage("email 형식을 확인해주세요"),
+
         validate
     ];
 
